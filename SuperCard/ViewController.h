6e66d2a653749cf7614a2ae6387dbc977c287e5c
Payton_戴鹏程 <dpc11@qq.com>
2015-07-26 16:22:16 +0800

    Initial Commit

---
+++ SuperCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by 布白 on 15/7/26.
+//  Copyright (c) 2015年 DPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SuperCard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SuperCard
+//
+//  Created by 布白 on 15/7/26.
+//  Copyright (c) 2015年 DPC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
